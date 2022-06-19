58de24ff7c784165ce758aa1f7b3504e0dfa1d78
AHMAD_KHATIB <ahmadkhatib923@gmail.com>
2022-06-19 21:41:19 +0300

    end structuring the ingredient and shopping list content

--- 2-shop-app/src/app/shopping-list/shopping-list.component.ts
+++ 2-shop-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.mode';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 4),
+    new Ingredient('tomatoes', 10),
+  ];
 }
