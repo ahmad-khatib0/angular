a09054f3e6c304d7053660014d0447133d283197
Ahmad-khatib <ahmadkhatib923@gmail.com>
2022-07-30 20:43:39 +0300

    services use, cross component communication using services

--- 5-services/src/app/app.component.ts
+++ 5-services/src/app/app.component.ts
@@ -1,33 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = '5-services';
-
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active',
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive',
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown',
-    },
-  ];
-
-  onAccountAdded(newAccount: { name: string; status: string }) {
-    this.accounts.push(newAccount);
+  constructor(private accountsService: AccountsService) {}
+  ngOnInit(): void {
+    this.accounts = this.accountsService.accounts;
   }
-
-  onStatusChanged(updateInfo: { id: number; newStatus: string }) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
-  }
+  accounts: { name: string; status: string }[] = [];
 }
