48dd53de637c6ccbbe071aa87e9c48656e4802df
Ahmad-khatib <ahmadkhatib923@gmail.com>
2022-07-30 13:17:06 +0300

    basic directive, and directive using Renderer2

--- 4-directives/src/app/app.module.ts
+++ 4-directives/src/app/app.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-hilight/basic-hilight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
