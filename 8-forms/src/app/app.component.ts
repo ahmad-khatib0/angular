a78eeda2993d41970b013f11f66cde418dec9df8
Ahmad-khatib <ahmadkhatib923@gmail.com>
2022-07-31 22:27:39 +0300

    more on forms

--- 8-forms/src/app/app.component.ts
+++ 8-forms/src/app/app.component.ts
@@ -8,15 +8,48 @@ import { NgForm } from "@angular/forms";
 })
 export class AppComponent {
   @ViewChild("f") signupForm: NgForm;
+  submitted: boolean = false;
   defaultQuestion = "pet";
   answer = "";
+  genders: string[] = ["female", "male"];
+
+  user = {
+    username: "",
+    email: "",
+    secret: "",
+    answer: "",
+    gender: "",
+  };
+
   suggestUserName() {
     const suggestedName = "Superuser";
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: "",
+    //   },
+    //   secret: "pet",
+    //   questionAnswer: "",
+    //   gender: "male",
+    // });//not best approach
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
   }
   // onSubmit(form: NgForm) {
   // console.log(form);
   // }
   onSubmit() {
     console.log(this.signupForm); //using ViewChild is useful ie if you wanna get data before submitting
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secret = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
