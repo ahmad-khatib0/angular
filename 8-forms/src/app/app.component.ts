9170af8762212019aba9fa0c8c48d61cd0aca943
Ahmad-khatib <ahmadkhatib923@gmail.com>
2022-07-31 21:52:46 +0300

    onSubmit handling, built-in validators , NgModel, ngModelGroup, built-in css invalid classes

--- 8-forms/src/app/app.component.ts
+++ 8-forms/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  @ViewChild("f") signupForm: NgForm;
+  defaultQuestion = "pet";
+  answer = "";
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    const suggestedName = "Superuser";
+  }
+  // onSubmit(form: NgForm) {
+  // console.log(form);
+  // }
+  onSubmit() {
+    console.log(this.signupForm); //using ViewChild is useful ie if you wanna get data before submitting
   }
 }
