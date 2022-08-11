5831b8683e51b0636bf571d839be3a7e454cdf5f
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-08-12 01:49:30 +0300

    add http functionallity to the shopping application

--- 2-shop-app/src/app/recipes/recipe.service.ts
+++ 2-shop-app/src/app/recipes/recipe.service.ts
@@ -9,26 +9,32 @@ export class RecipeService {
   constructor(private shoppingListService: ShoppingListService) {}
   recipeChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'test',
-      'this is test description 1',
-      'https://thumbs.dreamstime.com/z/chef-recipe-box-2270380.jpg',
-      [new Ingredient('Buns', 1), new Ingredient('Meet', 4)]
-    ),
-    new Recipe(
-      'test',
-      'this is test description 2',
-      'https://thumbs.dreamstime.com/z/chef-recipe-box-2270380.jpg',
-      [new Ingredient('fish', 1), new Ingredient('yummy', 4)]
-    ),
-    new Recipe(
-      'test',
-      'this is test description 3',
-      'https://thumbs.dreamstime.com/z/chef-recipe-box-2270380.jpg',
-      [new Ingredient('meet', 1), new Ingredient('Eggs', 4)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'test',
+  //     'this is test description 1',
+  //     'https://thumbs.dreamstime.com/z/chef-recipe-box-2270380.jpg',
+  //     [new Ingredient('Buns', 1), new Ingredient('Meet', 4)]
+  //   ),
+  //   new Recipe(
+  //     'test',
+  //     'this is test description 2',
+  //     'https://thumbs.dreamstime.com/z/chef-recipe-box-2270380.jpg',
+  //     [new Ingredient('fish', 1), new Ingredient('yummy', 4)]
+  //   ),
+  //   new Recipe(
+  //     'test',
+  //     'this is test description 3',
+  //     'https://thumbs.dreamstime.com/z/chef-recipe-box-2270380.jpg',
+  //     [new Ingredient('meet', 1), new Ingredient('Eggs', 4)]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipeChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
