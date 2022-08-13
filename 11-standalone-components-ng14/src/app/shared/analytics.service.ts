06fed6e05ca9fe16413ad88c5afa1707d1736ecc
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-08-14 00:10:49 +0300

    standalone with routing

--- 11-standalone-components-ng14/src/app/shared/analytics.service.ts
+++ 11-standalone-components-ng14/src/app/shared/analytics.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-@Injectable({ providedIn: 'root' })
+// @Injectable({ providedIn: 'root' })//first way, second way in main.ts
 export class AnalyticsService {
   registerClick() {
-    console.log('Clicked!');
+    console.log("Clicked!");
   }
 }
