9359abb6ab17a47db459dd1e52c2d8422422ea58
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-08-13 17:40:08 +0300

    extract auth module

--- 2-shop-app/src/app/app.module.ts
+++ 2-shop-app/src/app/app.module.ts
@@ -1,40 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app.routing.module';
-import { RecipeService } from './recipes/recipe.service';
-import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipesModule } from './recipes.module';
 import { ShoppingListModule } from './shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, AuthComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     RecipesModule,
     ShoppingListModule,
+    AuthModule,
     SharedModule,
+    CoreModule,
   ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true,
-    },
-  ],
+  providers: [],
   bootstrap: [AppComponent],
   // entryComponents : [ AlertComponent ]//for angular < 9 in order for createComponent to work
 })
