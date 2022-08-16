6f56f669073fea4e1d9fdc7158c7c6477da2d262
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-08-17 01:23:10 +0300

    basic and advanced use cases

--- 14-animations/src/app/app.component.ts
+++ 14-animations/src/app/app.component.ts
@@ -1,13 +1,114 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  animations: [
+    trigger("divState", [
+      state(
+        "normal",
+        style({
+          "background-color": "red",
+          transform: "translateX(0)",
+        })
+      ),
+      state(
+        "highlighted",
+        style({
+          backgroundColor: "blue",
+          transform: "translateX(100px)",
+        })
+      ),
+      transition("normal => highlighted", animate(300)),
+      transition("highlighted => normal", animate(800)), //or
+      // transition("highlighted <=> normal", animate(800)), //both directions
+    ]),
+    trigger("wildState", [
+      state(
+        "normal",
+        style({
+          "background-color": "red",
+          transform: "translateX(0) scale(1)",
+        })
+      ),
+      state(
+        "highlighted",
+        style({
+          backgroundColor: "blue",
+          transform: "translateX(100px) scale(1)",
+        })
+      ),
+      state(
+        "shrunken",
+        style({
+          backgroundColor: "green",
+          transform: "translateX(0px) scale(0.5)",
+        })
+      ),
+      transition("normal => highlighted", animate(300)),
+      transition("highlighted => normal", animate(800)),
+      transition("shrunken <=> *", [
+        style({ "background-color": "orang" }), //staring phase
+        animate(
+          1000,
+          style({
+            borderRadius: "50px",
+          })
+        ), //in between animating
+        animate(500), //end phase
+      ]), // * any state
+    ]),
+    trigger("list1", [
+      state(
+        "in",
+        style({
+          opacity: 1,
+          transform: "translateX(0)",
+        })
+      ),
+      transition("void => *", [
+        // in this case, here the start phase is the transition function, because we'll begin from void
+        style({ opacity: 0, transform: "translateX(-100px)" }),
+        animate(800),
+      ]),
+      transition("* => void", [
+        animate(300, style({ transform: "translateX(100px)", opacity: 0 })),
+      ]),
+      // void when its not appearing (reserved word)
+    ]),
+  ],
 })
 export class AppComponent {
-  list = ['Milk', 'Sugar', 'Bread'];
+  list = ["Milk", "Sugar", "Bread"];
+  state = "normal";
+  wildState = "normal";
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onAnimate() {
+    this.state === "normal"
+      ? (this.state = "highlighted")
+      : (this.state = "normal");
+
+    this.wildState === "normal"
+      ? (this.wildState = "highlighted")
+      : (this.wildState = "normal");
+  }
+
+  onShrink() {
+    this.wildState = "shrunken";
+  }
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+  onDelete(item) {
+    console.log(item);
+    this.list.splice(item, 1);
+  }
 }
