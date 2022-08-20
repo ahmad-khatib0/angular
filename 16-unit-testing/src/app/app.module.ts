84c38fef7252c1b21f377671826311ddf1ce1f51
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-08-20 11:56:35 +0300

    normal test, and async tests

--- 16-unit-testing/src/app/app.module.ts
+++ 16-unit-testing/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UserComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
